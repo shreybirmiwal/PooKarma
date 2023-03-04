520ecbf76c1bda33d67482b8ebb3b4e75e5281df
Shrey <shreybirmiwal@gmail.com>
2023-03-04 12:18:51 -0600

    not working but pushed

--- firebase.js
+++ firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +16,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth();
--- screens/Map.js
+++ screens/Map.js
@@ -1,19 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet } from "react-native";
 import MapView from "react-native-maps";
 import { Marker, Callout } from 'react-native-maps';
 import { View, Text, Image } from "react-native";
 
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import { auth, db } from '../firebase';
+import { useState } from "react";
+import { Button } from "react-native/Libraries/Components/Button";
+
+const getData = async () => {
+
+  const querySnapshot = await getDocs(collection(db, "poops"));
+  var row = []
+
+  querySnapshot.forEach((doc) => {
+
+    //console.warn(doc.data().long, " ", doc.data().lat , " " , doc.data());
+    row.push({lat : doc.data().lat, long : doc.data().long, poster : doc.data().poster, image : doc.data().image})
+  
+  });
+
+  
+  return row
+
+}
+
+
 const Map = () => {
+
+  //const [data, setData] = useState([]);
+  //const [loaded, setLoading] = useState(false);
+
+  //useEffect(() => {
+    //setData(getData())
+    //setData([{lat: 0, long:1, poster:"s", image:"s"}])
+    //setLoading(true)
+
+  //}, []);
+
+  const [mapPoints, setMapPoints] = useState([{lat: 0, long:1, poster:"s", image:"s"}, {lat: 0, long:1, poster:"s", image:"s"}, {lat: 0, long:1, poster:"s", image:"s"},{lat: 0, long:1, poster:"s", image:"s"},{lat: 0, long:1, poster:"s", image:"s"}])
+
 
   return (
     <View>
+
+      <Button
+        title="testing"
+        onPress = {checkStatus()}
+      />
+      
         <MapView style = {styles.map}
           initialRegion={{
           latitude: 30.78825,
           longitude: -100.4324,
           latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,}}>
+          longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+          followsUserLocation={true}
+          >
 
 
           <Marker coordinate={{ latitude : 30 , longitude : -100 }}>
@@ -21,12 +67,14 @@ const Map = () => {
               <View>
                 <Image source={{uri: 'https://www.pinclipart.com/picdir/big/52-527957_dog-health-monitoring-by-keeping-track-of-poop.png'}} style={styles.picture}/>
                 <Text styles={styles.poster}> Shrey Birmiwal </Text>
+
               </View>
             </Callout>
           </Marker>
             
 
         </MapView>
+        
     </View>
   );
 };
