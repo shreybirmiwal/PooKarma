afaf179541e645785f4d24217a73126470af4c43
Shrey <shreybirmiwal@gmail.com>
2023-03-04 13:52:17 -0600

    working SPLASH SCREEN

--- screens/Splash.js
+++ screens/Splash.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const Splash = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.logoContainer}>
+      <View >
         <Image
           source={require('../assets/pookarma-logo.png')}
           style={styles.logo}
         />
       </View>
-      <View style={styles.spinnerContainer}>
-        <FontAwesomeIcon icon={faSpinner} size={80} color="#8B4513" spin />
-      </View>
+
       <View style={styles.buttonContainer}>
         <TouchableOpacity style={styles.signUpButton}>
           <Text style={styles.buttonText}>Sign Up</Text>
@@ -30,27 +26,24 @@ const Splash = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5DEB3',
+    backgroundColor: '#FFFF',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  logoContainer: {
-    marginTop: 50,
-  },
+
   logo: {
-    width: 150,
-    height: 150,
+    width: 300,
+    height: 300,
     resizeMode: 'contain',
   },
-  spinnerContainer: {
-    marginTop: 50,
-  },
+
   buttonContainer: {
     position: 'absolute',
     bottom: 0,
-    flexDirection: 'row',
+    flexDirection: 'column',
     width: '100%',
-    paddingBottom: 30,
+    paddingBottom: 40,
+    
   },
   signUpButton: {
     flex: 1,
@@ -62,6 +55,7 @@ const styles = StyleSheet.create({
   },
   signInButton: {
     flex: 1,
+    marginTop: 10,
     backgroundColor: '#F5DEB3',
     borderRadius: 10,
     marginHorizontal: 10,
